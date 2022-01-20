8529911d3ff20374855ab4fe849c81ae65790bdc
Ezeobi Uchenna <78647544+jay-uchenna@users.noreply.github.com>
2022-01-20 10:25:54 +0300

    Initial Commit

---
+++ learing functors/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  learing functors
+//
+//  Created by Jedidiah Uchenna on 20.01.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
